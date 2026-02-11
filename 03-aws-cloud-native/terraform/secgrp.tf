2822faca2bc573472a60ce6571d1acd84fb7f478
amr <amrmedhatamer100@gmail.com>
2026-02-11 12:50:12 +0200

    edit port number in security group to 80 from 8080 for Tomcat-SG

--- 03-aws-cloud-native/terraform/secgrp.tf
+++ 03-aws-cloud-native/terraform/secgrp.tf
@@ -32,7 +32,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_frontend" 
 
 resource "aws_security_group" "Tomcat-SG" {
   name        = "Tomcat-SG"
-  description = "Allow 8080 inbound traffic from Frontend and all outbound traffic"
+  description = "Allow 80 inbound traffic from Frontend and all outbound traffic"
   vpc_id      = module.vpc.vpc_id
 
   tags = {
@@ -48,12 +48,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_tomcat" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_8080_from_frontend" {
+resource "aws_vpc_security_group_ingress_rule" "allow_80_from_frontend" {
   security_group_id            = aws_security_group.Tomcat-SG.id
   referenced_security_group_id = aws_security_group.Load-Balancer-SG.id
-  from_port                    = 8080
+  from_port                    = 80
   ip_protocol                  = "tcp"
-  to_port                      = 8080
+  to_port                      = 80
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_tomcat" {
