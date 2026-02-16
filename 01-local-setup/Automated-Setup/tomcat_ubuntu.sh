ee69406d5fb2b02b3145753e0223acb121c743ce
amr <amrmedhatamer100@gmail.com>
2026-02-16 14:52:52 +0200

    try to make src folder in root of the project is single source of truth for all code and configuration, and remove any duplicate files in the 01-local-setup, 02-aws-lift-shift, and 03-aws-cloud-native folders. This way, we can ensure that any changes made to the code or configuration are reflected across all environments without having to maintain multiple copies of the same files. Additionally, we can update the Vagrantfile and tomcat_ubuntu.sh scripts to reference the src folder instead of the individual environment folders. This will help streamline our development process and reduce the chances of inconsistencies between environments.

--- 01-local-setup/Automated-Setup/tomcat_ubuntu.sh
+++ 01-local-setup/Automated-Setup/tomcat_ubuntu.sh
@@ -4,3 +4,5 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install openjdk-8-jdk -y
 sudo apt install tomcat8 tomcat8-admin tomcat8-docs tomcat8-common git -y
+cd /vagrant/src 
+mvn clean install 
