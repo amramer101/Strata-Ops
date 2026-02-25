bae966aa1eaeeeac71ace8f6e2111b8003d6da4a
amr <amr@gmail.coo>
2026-02-25 13:58:52 +0200

    delete S3 bucket and IAM role for EC2 instances, and update security group rules and user data scripts for Tomcat and MySQL EC2 instances.

--- 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
+++ 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
@@ -7,7 +7,6 @@ sudo apt install openjdk-17-jdk -y
 
 sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
 
-sudo snap install aws-cli --classic
 
 sudo rm -rf /var/lib/tomcat10/webapps/ROOT
 
