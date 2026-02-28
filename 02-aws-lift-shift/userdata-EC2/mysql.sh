341db332501e3a81c5f9c950767ce17d9dc80e3d
amr <amr@gmail.coo>
2026-02-28 12:44:49 +0200

    correct the mysql.sh script to install mysql server instead of mariadb server

--- 02-aws-lift-shift/userdata-EC2/mysql.sh
+++ 02-aws-lift-shift/userdata-EC2/mysql.sh
@@ -16,17 +16,17 @@ DATABASE_PASS=$(aws ssm get-parameter --name "/strata-ops/mysql-password" \
   --with-decryption --query "Parameter.Value" --output text --region $REGION)
 
 echo "Configuring MariaDB..."
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS accounts;"
-mysql -u root -e "GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'localhost' IDENTIFIED BY '$DATABASE_PASS';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'%' IDENTIFIED BY '$DATABASE_PASS';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS accounts;"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'localhost' IDENTIFIED BY '$DATABASE_PASS';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'%' IDENTIFIED BY '$DATABASE_PASS';"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
 
 echo "Restoring Database Dump..."
 cd /tmp
 git clone -b main https://github.com/amramer101/Strata-Ops.git
-mysql -u root accounts < /tmp/Strata-Ops/src/main/resources/db_backup.sql
+sudo mysql -u root accounts < /tmp/Strata-Ops/src/main/resources/db_backup.sql
 
 echo "Verifying..."
-mysql -u root accounts -e "SHOW TABLES;"
+sudo mysql -u root accounts -e "SHOW TABLES;"
 
 echo "MySQL Provisioning Completed Successfully!"
