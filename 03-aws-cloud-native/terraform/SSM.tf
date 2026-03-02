f37d5580faea5c6109a1d5b6b7f28e4da5757897
amr <amr@gmail.coo>
2026-03-02 19:47:34 +0200

    add sonar token to terraform variables and to ssm parameters

--- 03-aws-cloud-native/terraform/SSM.tf
+++ 03-aws-cloud-native/terraform/SSM.tf
@@ -1,6 +1,6 @@
 resource "random_password" "db_password" {
-  length  = 8
-  special = false
+  length           = 8
+  special          = false
 }
 
 resource "aws_ssm_parameter" "mysql_password" {
@@ -11,8 +11,8 @@ resource "aws_ssm_parameter" "mysql_password" {
 
 
 resource "random_password" "rmq_password" {
-  length  = 8
-  special = false
+  length           = 8
+  special          = false
 }
 
 resource "aws_ssm_parameter" "rabbitmq_password" {
@@ -21,3 +21,20 @@ resource "aws_ssm_parameter" "rabbitmq_password" {
   value = random_password.rmq_password.result
 }
  
+resource "aws_ssm_parameter" "sonar_token" {
+  name  = "/strata-ops/sonar-token"
+  type  = "SecureString"
+  value = var.sonar_token
+}
+
+resource "aws_ssm_parameter" "sonar_org" {
+  name  = "/strata-ops/sonar-org"
+  type  = "String"
+  value = var.sonar_organization
+}
+
+resource "aws_ssm_parameter" "sonar_project" {
+  name  = "/strata-ops/sonar-project"
+  type  = "String"
+  value = var.sonar_project_key
+}
