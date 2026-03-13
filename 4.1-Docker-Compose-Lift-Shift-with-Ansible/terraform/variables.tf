fd7f8f9042ac0363413e38e02ac4072328691433
amr <amr@gmail.coo>
2026-03-13 22:32:43 +0200

    add terraform files for VPC, security group, and EC2 instance; update .gitignore and docker-compose.yml

---
+++ 4.1-Docker-Compose-Lift-Shift-with-Ansible/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "AWS_Region" {
+  default = "eu-central-1"
+}
+
+variable "VPC_Name" {
+  default = "Eprofile-VPC-docker"
+}
+
+variable "VPC_CIDR" {
+  default = "10.0.0.0/16"
+}
+
+variable "Public_Subnet_CIDR" {
+  default = ["10.0.0.0/24"]
+}
+
+variable "AWS_Zone-a" {
+  default = "eu-central-1a"
+}
+
+####  Key Pair Variables
+
+variable "Pub_Key_Path" {
+  default = "docker-key.pub"
+}
+
+
