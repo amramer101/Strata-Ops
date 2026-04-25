437dd993573c51cb14c8e3f05c94576df54dfc92
amr <amr@gmail.coo>
2026-04-25 18:26:26 +0300

    correct security group reference in ingress rules and update provider versions

--- 6.1-EKS-Provisioning-with-Push-Based-CICD/terraform/alb-controller.tf
+++ 6.1-EKS-Provisioning-with-Push-Based-CICD/terraform/alb-controller.tf
@@ -25,33 +25,6 @@ module "alb_irsa_role" {
   }
 }
 
-data "http" "alb_policy" {
-  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
-}
-
-resource "aws_iam_policy" "alb_controller_policy" {
-  name   = "strata-AWSLoadBalancerControllerIAMPolicy"
-  policy = data.http.alb_policy.response_body
-}
-
-module "alb_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
-
-  role_name = "strata-alb-controller-role"
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    }
-  }
-
-  role_policy_arns = {
-    alb_policy = aws_iam_policy.alb_controller_policy.arn
-  }
-}
-
 resource "helm_release" "alb_controller" {
   name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
