2f760591ef3507930355feea358426aca26f770a
Amr Medhat Amer <amrmedhatamer100@gmail.com>
2026-05-21 16:41:37 +0300

    add backend.tf for remote state management in Azure

--- 7-Azure-Lift-Shift-with-CICD-Migration/terraform/backend.tf
+++ 7-Azure-Lift-Shift-with-CICD-Migration/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state"
+    resource_group_name  = "terraform-rg"
     storage_account_name = "terraformstateeprofile"       
     container_name       = "tfstate"               
     key                  = "terraform.tfstate"     
